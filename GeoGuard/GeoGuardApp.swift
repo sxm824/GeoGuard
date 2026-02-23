c2f1c357ee30af176bd4c261d33e6dba4215c17d
Sal Mukbil <smukbil@gmail.com>
2026-02-23 21:59:40 +0000

    Initial Commit

---
+++ GeoGuard/GeoGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoGuardApp.swift
+//  GeoGuard
+//
+//  Created by Saleh Mukbil on 2026-02-23.
+//
+
+import SwiftUI
+
+@main
+struct GeoGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
